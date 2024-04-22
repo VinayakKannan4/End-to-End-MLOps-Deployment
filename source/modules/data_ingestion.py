e2e6b71a1c092b616d57f03bdc4b2f432a1431ac
VinayakKannan4 <vk297@georgetown.edu>
2024-04-22 10:25:56 -0400

    Model Training and Evaluation

--- source/modules/data_ingestion.py
+++ source/modules/data_ingestion.py
@@ -12,6 +12,9 @@ from source.logs import logging
 from source.modules.data_transformation import DataTransformation
 from source.modules.data_transformation import DataTransformationConfig
 
+from source.modules.model_trainer import ModelTrainerConfig
+from source.modules.model_trainer import ModelTrainer
+
 ## Creating class to save raw, train and test data
 @dataclass
 class DataIngestionConfig:
@@ -58,3 +61,5 @@ if __name__ == "__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
     
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
